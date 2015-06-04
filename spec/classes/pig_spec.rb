e5c598a37d2bb29782635927024b498b49ee11ff
František Dvořák <valtri@civ.zcu.cz>
2015-06-04 14:42:55 +0200

    Moar unit-tests (profile files, datafu package).

--- spec/classes/pig_spec.rb
+++ spec/classes/pig_spec.rb
@@ -1,18 +1,5 @@
 require 'spec_helper'
 
-describe 'pig::config', :type => 'class' do
-  $test_os.each do |facts|
-    os = facts['operatingsystem']
-
-    context "on #{os}" do
-      let(:facts) do
-        facts
-      end
-      it { should compile.with_all_deps }
-    end
-  end
-end
-
 describe 'pig', :type => 'class' do
   $test_os.each do |facts|
     os = facts['operatingsystem']
@@ -23,6 +10,24 @@ describe 'pig', :type => 'class' do
       end
       it { should compile.with_all_deps }
       it { should contain_package('pig') }
+      if os == 'Fedora' then
+        it { should contain_file('/etc/profile.d/hadoop-pig.sh').with_ensure_absent }
+        it { should contain_file('/etc/profile.d/hadoop-pig.csh').with_ensure_absent }
+      else
+        it { should contain_file('/etc/profile.d/hadoop-pig.sh') }
+        it { should contain_file('/etc/profile.d/hadoop-pig.csh') }
+      end
+    end
+
+    context "on #{os} with datafu" do
+      let(:facts) do
+        facts
+      end
+      let(:params) { {:datafu_enabled => true} }
+      it { should compile.with_all_deps }
+      if os != 'Fedora' then
+        it { should contain_package('pig-udf-datafu') }
+      end
     end
   end
 end
